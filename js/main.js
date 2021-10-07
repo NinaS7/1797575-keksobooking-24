aa703c9284a4a52f1ca499fdb72440a564373da2
Нина Стафеева <rugacheva@mail.ru>
2021-10-07 15:12:39 +0300

    Больше функций

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-// Выполнено с использованием: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+/* Выполнено с использованием:
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+https://learn.javascript.ru/number
+*/
 
 // 1.Функция, возвращающая случайное целое число из переданного диапазона включительно.
 
