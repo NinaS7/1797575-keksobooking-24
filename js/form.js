37a1f5612f4e86fc157502193f8016950cc685a3
Нина Стафеева <rugacheva@mail.ru>
2021-11-10 15:03:00 +0300

    Правда или действие (часть 1)

--- js/form.js
+++ js/form.js
@@ -1,5 +1,22 @@
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+const MAX_ROOMS = 100;
+
+const MIN_PRICE_TYPES = {
+  bungalow: '0',
+  flat: '1000',
+  hotel: '3000',
+  house: '5000',
+  palace: '10000',
+};
+
 const adForm = document.querySelector('.ad-form');
 const mapFilters = document.querySelector('.map__filters');
+const titleInput = adForm.querySelector('#title');
+const price = adForm.querySelector('#price');
+const type = adForm.querySelector('#type');
+const roomNumber = adForm.querySelector('#room_number');
+const capacity = adForm.querySelector('#capacity');
 
 const getPageDisabled = () => {
   adForm.classList.add('ad-form--disabled');
@@ -28,5 +45,62 @@ const getPageActive = () => {
     children.removeAttribute('disabled', 'disabled');
   });
 };
+
+titleInput.addEventListener('input', () => {
+  titleInput.setCustomValidity('');
+  if (titleInput.value.length < MIN_TITLE_LENGTH) {
+    titleInput.setCustomValidity(`Заголовок должен состоять минимум из ${MIN_TITLE_LENGTH} символов`);
+    titleInput.style = 'border: 2px solid red';
+  } else
+  if (titleInput.value.length > MAX_TITLE_LENGTH) {
+    titleInput.setCustomValidity(`Заголовок не должен превышать ${MAX_TITLE_LENGTH} символов`);
+    titleInput.style = 'border: 2px solid red';
+  }
+  titleInput.reportValidity();
+});
+
+type.addEventListener('change', () => {
+  const getMinPrice = () => {
+    price.min = MIN_PRICE_TYPES[type.value];
+    price.placeholder = MIN_PRICE_TYPES[type.value];
+  };
+  price.setCustomValidity('');
+  if (Number(price.value) < Number(MIN_PRICE_TYPES[type.value])) {
+    price.setCustomValidity(`Минимальная цена для выбранного типа жилья должна быть не менее ${MIN_PRICE_TYPES[type.value]} ₽/ночь`);
+    price.style = 'border: 2px solid red';
+  } else
+  if (Number(price.value) > Number(price.max)) {
+    price.setCustomValidity(`Цена не должна превышать ${price.max}`);
+    price.style = 'border: 2px solid red';
+  }
+  price.reportValidity();
+  getMinPrice();
+});
+
+roomNumber.addEventListener('change', () => {
+  roomNumber.setCustomValidity('');
+  if (Number(roomNumber.value) < Number(capacity.value)) {
+    roomNumber.setCustomValidity('Количество комнат должно быть не менее количества гостей');
+    roomNumber.style = 'border: 2px solid red';
+  } else
+  if (Number(roomNumber.value) === MAX_ROOMS && Number(capacity.value) !== 0) {
+    capacity.setCustomValidity('Вы выбрали вариант недвижимости "не для гостей"');
+    capacity.style = 'border: 2px solid red';
+  }
+  roomNumber.reportValidity();
+});
+
+capacity.addEventListener('change', () => {
+  capacity.setCustomValidity('');
+  if (Number(capacity.value) > Number(roomNumber.value)) {
+    capacity.setCustomValidity('Количество гостей должно быть не более количества комнат');
+    capacity.style = 'border: 2px solid red';
+  } else
+  if (Number(capacity.value) === 0 && Number(roomNumber.value) !== MAX_ROOMS) {
+    capacity.setCustomValidity('Укажите количество гостей');
+    capacity.style = 'border: 2px solid red';
+  }
+  capacity.reportValidity();
+});
 
 export {getPageDisabled, getPageActive};
